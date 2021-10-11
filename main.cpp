3e6133072a004cb5448718cc256519ed8d69a23b
VovaGlh <viggiv01@gmail.com>
2021-10-12 00:35:43 +0300

    Было написано два теста по требованиям заказчика

--- main.cpp
+++ main.cpp
@@ -2,11 +2,21 @@
 // Created by VovaGlh on 11.10.2021.
 //
 #include "multMatrixBlock.cpp"
-
+//#include "Test.cpp"
 using namespace std;
 int  main(){
-    inputMatrix();
-    multMatrixSqBad();
-    sumMatrix();
+    //inputMatrix();
+    //multMatrixSqBad();
+    //sumMatrix();
+    if(Test1()==0){
+        cout<<"\nTest1 = ok\n";
+    }else{
+        cout<<"\nTest1 = false\n";
+    }
+    if(Test2()==0){
+        cout<<"\nTest2 = ok\n";
+    }else{
+        cout<<"\nTest2 = false\n";
+    }
     return 0;
 }
--- multMatrixBlock.cpp
+++ multMatrixBlock.cpp
@@ -6,24 +6,17 @@
 using namespace std;
 int row1, row2, col1, col2;
 double** a, ** b, ** c;
+
 void inputMatrix(){
     cout<<"Hello!!!\n This program multiplies and sums the entered matrices.\nStart work.\n";
     cout << "\nEnter the number of rows of the first matrix: ";
-    cin >> row1;
+    //cin >> row1;
     cout << "\nEnter the number of columns of the first matrix: ";
-    cin >> col1;
+    //cin >> col1;
     cout << "\nEnter the number of rows of the second matrix: ";
-    cin >> row2;
+    //cin >> row2;
     cout << "\nEnter the number of columns of the second matrix: ";
-    cin >> col2;
-}
-int multMatrixSqBad(){
-    if (col1 != row2)
-    {
-        cout << "\nMultiplication is impossible!";
-        cin.get(); cin.get();
-        return 0;
-    }
+   // cin >> col2;
     // Ввод элементов первой матрицы
     a = new double* [row1];
     cout << "\nEnter the elements of the first matrix" << endl;
@@ -63,6 +56,45 @@ int multMatrixSqBad(){
             cout << b[i][j] << " ";
         }
         cout << endl;
+    }
+}
+void testInput(int row_t1,int row_t2,int col_t1,int col_t2){
+    row1=row_t1;
+    col1=col_t1;
+    row2=row_t2;
+    col2=col_t2;
+    // Ввод элементов первой матрицы
+    a = new double* [row1];
+    cout << "\nEnter the elements of the first matrix" << endl;
+    for (int i = 0; i < row1; i++)
+    {
+        a[i] = new double[col1];
+        for (int j = 0; j < col1; j++)
+        {
+            cout << "a[" << i << "][" << j << "]= ";
+            a[i][j]=3;
+        }
+    }
+    // Ввод элементов второй матрицы
+    b = new double* [row2];
+    cout << "\nEnter the elements of the second matrix" << endl;
+    for (int i = 0; i < row2; i++)
+    {
+        b[i] = new double[col2];
+        for (int j = 0; j < col2; j++)
+        {
+            cout << "b[" << i << "][" << j << "]= ";
+            b[i][j]=3;
+        }
+    }
+}
+
+int multMatrixSqBad(){
+    if (col1 != row2)
+    {
+        cout << "\nMultiplication is impossible!";
+        cin.get(); cin.get();
+        return 0;
     }
     // Умножение матриц
     c = new double* [row1];
@@ -107,4 +139,20 @@ int sumMatrix() {
             }
         }
     return 0;
+}
+int Test1(){
+    testInput(2,1,3,2);
+    if(multMatrixSqBad()==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+int Test2(){
+    testInput(3,4,4,4);
+    if(sumMatrix()==0){
+        return 0;
+    }else{
+        return 1;
+    }
 }
